7f3bbd386555467d9c2228e3ececebe2fce3f710
Tyler mui <tylermui1103@gmail.com>
2023-11-12 18:04:08 -0500

    new font, gitignore file, working on js

--- static/index.js
+++ static/index.js
@@ -1,37 +1,32 @@
 // JavaScript code for handling API requests and displaying data will go here????
-require('dotenv').config();
 
-const apiKey = process.env.API_KEY;
-const apiId = process.env.API_ID;
 var query = "";
 
-const apiUrl = `https://api.edamam.com/api/recipes/v2?type=public&q=${query}&app_id=${apiId}&app_key=${apiKey}`;
+// const apiUrl = `https://api.edamam.com/api/recipes/v2?type=public&q=${query}&app_id=${apiId}&app_key=${apiKey}&to=20`;
   
-fetch(apiUrl)
-  .then(response => {
-    // Check if the response status is okay (200)
-    if (response.ok) {
-      // Parse the JSON response data
-      return response.json();
-    }
-    // If response status is not okay, throw an error
-    throw new Error('Network response was not ok.');
-  })
-  .then(data => {
-    // Handle the data from the API
-    console.log(data);
-    // You can manipulate and display the data here
-  })
-  .catch(error => {
-    // Handle errors that occurred during the fetch operation
-    console.error('There has been a problem with your fetch operation:', error);
-  });
+// fetch(apiUrl)
+//   .then(response => {
+//     // Check if the response status is okay (200)
+//     if (response.ok) {
+//       // Parse the JSON response data
+//       return response.json();
+//     }
+//     // If response status is not okay, throw an error
+//     throw new Error('Network response was not ok.');
+//   })
+//   .then(data => {
+//     // Handle the data from the API
+//     console.log(data);
+//     // You can manipulate and display the data here
+//   })
+//   .catch(error => {
+//     // Handle errors that occurred during the fetch operation
+//     console.error('There has been a problem with your fetch operation:', error);
+//   });
 
 
 document.getElementById('searchButton').addEventListener('click', function() {
   // Grab the input value
   var foodInputValue = document.getElementById('foodInput').value;
 
-  // Store it in a variable or use it as needed
-  console.log('User input:', foodInputValue);
 });
