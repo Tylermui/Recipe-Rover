7e8e3c18e40337a68485c0ace8768b3b1a2ec6e7
Tyler mui <tylermui1103@gmail.com>
2023-11-12 21:22:42 -0500

    majority of the JS to work from API

--- static/index.js
+++ static/index.js
@@ -1,32 +1,48 @@
 // JavaScript code for handling API requests and displaying data will go here????
-
+import {API_ID} from './config.js'
+import {API_KEY} from './config.js'
 var query = "";
 
-// const apiUrl = `https://api.edamam.com/api/recipes/v2?type=public&q=${query}&app_id=${apiId}&app_key=${apiKey}&to=20`;
-  
-// fetch(apiUrl)
-//   .then(response => {
-//     // Check if the response status is okay (200)
-//     if (response.ok) {
-//       // Parse the JSON response data
-//       return response.json();
-//     }
-//     // If response status is not okay, throw an error
-//     throw new Error('Network response was not ok.');
-//   })
-//   .then(data => {
-//     // Handle the data from the API
-//     console.log(data);
-//     // You can manipulate and display the data here
-//   })
-//   .catch(error => {
-//     // Handle errors that occurred during the fetch operation
-//     console.error('There has been a problem with your fetch operation:', error);
-//   });
+$(document).ready(function () {
+  $('#searchButton').click(function (event) {
+      // Prevent the default form submission
+      event.preventDefault();
+      
+      //grabbing the query from the user
+      query = document.getElementById('foodInput').value;
+      console.log(query)
+      //where we are going to parse the api information
+      fetchAPI()
 
-
-document.getElementById('searchButton').addEventListener('click', function() {
-  // Grab the input value
-  var foodInputValue = document.getElementById('foodInput').value;
-
+      // Add your logic for handling the form submission here
+      // For example, you can trigger an AJAX request to fetch and display the recipe data
+      // based on the input in the 'foodInput' field.
+  });
 });
+
+async function fetchAPI() {
+  let apiUrl = `https://api.edamam.com/api/recipes/v2?type=public&q=${query}&app_id=${API_ID}&app_key=${API_KEY}&to=20`;
+  const response = await fetch(apiUrl);
+  const data = await response.json();
+  populateContainer(data.hits);
+  console.log(data);
+}
+
+function populateContainer(results) {
+  let recipeItem = "";
+  results.map((result) => {
+    recipeItem += `
+    <div class="recipeItem col-md-3 mx-5">
+        <img src="${result.recipe.image}" alt="">
+        <div class="flex-container">
+            <h2 class="title">${result.recipe.label}</h2>
+            <a href="${result.recipe.url}" class="viewButton">View Recipe</a>
+        </div>
+        <p class="Item-data p-1">
+            calories: ${result.recipe.calories.toFixed(0)}
+        </p>
+    </div>`;
+  });
+
+  $(".api-container").html(recipeItem);
+}
